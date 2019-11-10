19a449f63d871142e5d2557463b4c42edbc00090
cristian-guerrero <guerrero.chavez.cristian@gmail.com>
2019-11-10 17:23:51 -0500

    added datepicker css

---
+++ CitasMedicasMVC/Controllers/CallController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CitasMedicasMVC.Controllers
+{
+    public class CallController : Controller
+    {
+        // GET: Call
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
