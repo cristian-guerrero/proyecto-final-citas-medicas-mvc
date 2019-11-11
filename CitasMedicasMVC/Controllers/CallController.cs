85649d33e213c70e25a5f6f8f68112036852af9d
cristian-guerrero <guererero.chavez.cristian@gmail.com>
2019-11-10 19:43:32 -0500

    added rest call and view

--- CitasMedicasMVC/Controllers/CallController.cs
+++ CitasMedicasMVC/Controllers/CallController.cs
@@ -1,17 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
+using System.Net.Http.Headers;
+using RestSharp;
+using Newtonsoft.Json;
 
 namespace CitasMedicasMVC.Controllers
 {
-    public class CallController : Controller
+  public class CallController : Controller
+  {
+    // GET: Call
+    public ActionResult Index()
     {
-        // GET: Call
-        public ActionResult Index()
-        {
-            return View();
-        }
+      var citas = callApiAsync();
+      return View(citas);
     }
-}
+
+    private List<citas> callApiAsync()
+    {
+      string url = "http://rest.proyecto/";
+
+      var client = new RestClient(url);
+      var request = new RestRequest("api/Citas", Method.GET);
+      request.AddHeader("Content-Type", "application/json");
+      // execute the request
+      IRestResponse response = client.Execute(request);
+      var content = response.Content;
+      var result = JsonConvert.DeserializeObject<List<citas>>(content);
+      return result ?? new List<citas>();
+    }
+  }
+  }
